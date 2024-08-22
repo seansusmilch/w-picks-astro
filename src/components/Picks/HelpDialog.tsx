453d2cc03ee7353ec70f362e0cea8ef7efcaec6e
seansusmilch <seantsusmilch@gmail.com>
2024-08-22 00:32:49 -0500

    Fix help dialog

--- src/components/Picks/HelpDialog.tsx
+++ src/components/Picks/HelpDialog.tsx
@@ -15,32 +15,28 @@ export function HelpDialog() {
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Picks FAQs</DialogTitle>
+          <DialogTitle className='text-2xl'>Picks FAQs</DialogTitle>
+          <DialogTitle>Can I delete my pick?</DialogTitle>
           <DialogDescription>
-            <h2 className='text-lg font-bold'>Can I delete my pick?</h2>
-            <p>
-              Yes, you can delete your pick by selecting the middle option in
-              between the two team logos. You can delete your pick at any time
-              before the game starts. Once the game starts, you cannot change
-              your pick.
-            </p>
-            <h2 className='text-lg font-bold'>How do I pick a team?</h2>
-            <p>
-              Click on the team you think will win the game. You can change your
-              pick at any time before the game starts.
-            </p>
-            <h2 className='text-lg font-bold'>Can I change my pick?</h2>
-            <p>
-              Yes, you can change your pick at any time before the game starts.
-              Once the game starts, you cannot change your pick.
-            </p>
-            <h2 className='text-lg font-bold'>
-              What happens if I don't pick a team?
-            </h2>
-            <p>
-              If you don't pick a team, you will not receive a W or L. There is
-              no penalty for not picking for a game.
-            </p>
+            Yes, you can delete your pick by selecting the middle option in
+            between the two team logos. You can delete your pick at any time
+            before the game starts. Once the game starts, you cannot change your
+            pick.
+          </DialogDescription>
+          <DialogTitle>How do I pick a team?</DialogTitle>
+          <DialogDescription>
+            Click on the team you think will win the game. You can change your
+            pick at any time before the game starts.
+          </DialogDescription>
+          <DialogTitle>Can I change my pick?</DialogTitle>
+          <DialogDescription>
+            Yes, you can change your pick at any time before the game starts.
+            Once the game starts, you cannot change your pick.
+          </DialogDescription>
+          <DialogTitle>What happens if I don't pick a team?</DialogTitle>
+          <DialogDescription>
+            If you don't pick a team, you will not receive a W or L. There is no
+            penalty for not picking for a game.
           </DialogDescription>
         </DialogHeader>
       </DialogContent>
