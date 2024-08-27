ee95b3d1c52640d01623170c91835e8514a64666
seansusmilch <seantsusmilch@gmail.com>
2024-08-26 22:57:12 -0500

    Try fix theme toggle

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -14,6 +14,8 @@ export function ThemeToggle() {
     'theme-light' | 'dark' | 'system'
   >('theme-light');
 
+  const [dropOpen, setDropOpen] = React.useState(false);
+
   React.useEffect(() => {
     const isDarkMode = document.documentElement.classList.contains('dark');
     setThemeState(isDarkMode ? 'dark' : 'theme-light');
@@ -28,8 +30,8 @@ export function ThemeToggle() {
   }, [theme]);
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+    <DropdownMenu open={dropOpen} onOpenChange={(val) => setDropOpen(val)}>
+      <DropdownMenuTrigger onClick={() => setDropOpen((val) => !val)} asChild>
         <Button variant='outline' size='icon'>
           <SunIcon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
           <MoonIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
