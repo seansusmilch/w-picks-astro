33d6d97a55042d8c3e90c8fa23ea7a51075273fd
Sean <seantsusmilch@gmail.com>
2024-08-29 01:27:55 -0500

    improve header

--- src/components/Header/UserDropdown.tsx
+++ src/components/Header/UserDropdown.tsx
@@ -12,7 +12,10 @@ export function UserDropdown({ user }: { user: any }) {
   const [dropOpen, setDropOpen] = useState(false);
   return (
     <DropdownMenu open={dropOpen} onOpenChange={(val) => setDropOpen(val)}>
-      <DropdownMenuTrigger onClick={() => setDropOpen((val) => !val)}>
+      <DropdownMenuTrigger
+        className='bg-gradient-to-r from-cyan-500 to-purple-500 inline-block text-transparent bg-clip-text'
+        onClick={() => setDropOpen((val) => !val)}
+      >
         {user.record.username}
       </DropdownMenuTrigger>
       <DropdownMenuContent>
