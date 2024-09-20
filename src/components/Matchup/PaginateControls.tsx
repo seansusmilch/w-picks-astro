dbe4f04a05e174423f87f3ec4ee615ba8cf08d65
Sean <seantsusmilch@gmail.com>
2024-09-20 13:56:32 -0500

    Added better pagination controls

---
+++ src/components/Matchup/PaginateControls.tsx
@@ -0,0 +1,37 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/components/ui/pagination';
+
+export function PaginateControls({
+  page,
+  totalPages,
+}: {
+  page: number;
+  totalPages: number;
+}) {
+  return (
+    <Pagination>
+      <PaginationContent className='w-full flex justify-between'>
+        <PaginationItem>
+          <PaginationPrevious
+            href={`/matchups/page/${page > 1 ? page - 1 : 1}`}
+          />
+        </PaginationItem>
+        <PaginationItem>
+          {page}/{totalPages}
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationNext
+            href={`/matchups/page/${page < totalPages ? page + 1 : totalPages}`}
+          />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
+}
