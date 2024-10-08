11a61f7354dbc2f53b7df865fc6edd4c0df4eba4
seansusmilch <seantsusmilch@gmail.com>
2024-10-08 00:48:24 -0500

    make user picks table nice

--- src/components/Profile/UserPicksTable.tsx
+++ src/components/Profile/UserPicksTable.tsx
@@ -9,7 +9,10 @@ import {
   TableRow,
 } from '@/components/ui/table';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import type { PickType } from '@/lib/definitions';
+import type { MatchupType, PickType } from '@/lib/definitions';
+import { TeamMap } from '@/components/NBA/teamMap';
+import { Logo } from '@/components/NBA/Logo';
+import moment from 'moment';
 
 export function UserPicksTable({
   picks,
@@ -45,15 +48,45 @@ export function UserPicksTable({
   );
 }
 
+function MatchupCell({ matchup }: { matchup: MatchupType }) {
+  const awayTeamShort =
+    TeamMap[matchup.away_code]?.name_short || matchup.away_code;
+  const homeTeamShort =
+    TeamMap[matchup.home_code]?.name_short || matchup.home_code;
+  return (
+    <TableCell>
+      <a href={`/matchups/${matchup.id}`} className='flex flex-col'>
+        <div className='flex gap-4'>
+          <Logo tricode={matchup.away_code} className='w-12 h-12' />
+          <Logo tricode={matchup.home_code} className='w-12 h-12' />
+        </div>
+        <p className='text-lg'>
+          {awayTeamShort} at {homeTeamShort}
+        </p>
+      </a>
+    </TableCell>
+  );
+}
+
+function DateCell({ date }: { date: Date }) {
+  return (
+    <TableCell>
+      <p className='text-xs text-muted-foreground'>
+        {moment(date).format('MMM D')}
+      </p>
+    </TableCell>
+  );
+}
+
 function PastPicksTable({ picks }: { picks: PickType[] }) {
   return (
     <Table>
       <TableHeader>
         <TableRow>
           <TableHead>W/L</TableHead>
+          <TableHead className='pr-4'>Date</TableHead>
           <TableHead>Matchup</TableHead>
-          <TableHead>Prediction</TableHead>
-          <TableHead>User</TableHead>
+          {/* <TableHead>Prediction</TableHead> */}
         </TableRow>
       </TableHeader>
       <TableBody>
@@ -61,18 +94,18 @@ function PastPicksTable({ picks }: { picks: PickType[] }) {
           <TableRow key={p.id}>
             <TableCell>
               <span
-                className={`text-sm font-bold ${
+                className={`font-bold ${
                   p.result === 'W' ? 'text-green-500' : 'text-red-500'
                 }`}
               >
                 {p.result}
               </span>
             </TableCell>
-            <TableCell className='font-medium'>
-              <a href={`/matchups/${p.matchup}`}>{p.matchup}</a>
-            </TableCell>
-            <TableCell>{p.win_prediction}</TableCell>
-            <TableCell>{p.user}</TableCell>
+            <DateCell date={p.expand.matchup.time_utc} />
+            <MatchupCell matchup={p.expand.matchup} />
+            {/* <TableCell className='flex items-start justify-end'>
+              <Logo tricode={p.win_prediction} className='w-12 h-12' />
+            </TableCell> */}
           </TableRow>
         ))}
       </TableBody>
@@ -86,16 +119,16 @@ function LivePicksTable({ picks }: { picks: PickType[] }) {
       <TableHeader>
         <TableRow>
           <TableHead>Matchup</TableHead>
-          <TableHead>Prediction</TableHead>
+          <TableHead className='text-right'>Prediction</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
         {picks.map((p) => (
           <TableRow key={p.id}>
-            <TableCell className='font-medium'>
-              <a href={`/matchups/${p.matchup}`}>{p.matchup}</a>
+            <MatchupCell matchup={p.expand.matchup} />
+            <TableCell className='flex items-start justify-end'>
+              <Logo tricode={p.win_prediction} className='w-12 h-12' />
             </TableCell>
-            <TableCell>{p.win_prediction}</TableCell>
           </TableRow>
         ))}
       </TableBody>
@@ -108,17 +141,19 @@ function UpcomingPicksTable({ picks }: { picks: PickType[] }) {
     <Table>
       <TableHeader>
         <TableRow>
+          <TableHead className='pr-4'>Date</TableHead>
           <TableHead>Matchup</TableHead>
-          <TableHead>Prediction</TableHead>
+          <TableHead className='text-right'>Prediction</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
         {picks.map((p) => (
           <TableRow key={p.id}>
-            <TableCell className='font-medium'>
-              <a href={`/matchups/${p.matchup}`}>{p.matchup}</a>
+            <DateCell date={p.expand.matchup.time_utc} />
+            <MatchupCell matchup={p.expand.matchup} />
+            <TableCell className='flex items-start justify-end'>
+              <Logo tricode={p.win_prediction} className='w-12 h-12' />
             </TableCell>
-            <TableCell>{p.win_prediction}</TableCell>
           </TableRow>
         ))}
       </TableBody>
