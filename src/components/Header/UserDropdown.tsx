6c3a78babf0a93516bbaddb4a05139ca9862096a
seansusmilch <seantsusmilch@gmail.com>
2025-01-23 11:54:49 -0600

    add user avatar to header

--- src/components/Header/UserDropdown.tsx
+++ src/components/Header/UserDropdown.tsx
@@ -7,18 +7,24 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { useState } from 'react';
+import { UserAvatar } from '@/components/Profile/UserAvatar';
+import { getUserAvatarUrl } from '@/lib/data_common';
 
 export function UserDropdown({ user }: { user: any }) {
   const [dropOpen, setDropOpen] = useState(false);
+  const avatarUrl = getUserAvatarUrl(user.record.id, user.record.avatar);
   return (
     <DropdownMenu open={dropOpen} onOpenChange={(val) => setDropOpen(val)}>
       <DropdownMenuTrigger
-        className='bg-gradient-to-r from-cyan-500 to-purple-500 inline-block text-transparent bg-clip-text'
+        className='flex gap-2 items-center'
         onClick={() => setDropOpen((val) => !val)}
       >
-        {user.record.username}
+        <UserAvatar avatar_url={avatarUrl} />
+        <span className='bg-gradient-to-r from-cyan-500 to-purple-500 inline-block text-transparent bg-clip-text'>
+          {user.record.username}
+        </span>
       </DropdownMenuTrigger>
-      <DropdownMenuContent>
+      <DropdownMenuContent className>
         <DropdownMenuLabel className='text-md'>My Account</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem asChild>
