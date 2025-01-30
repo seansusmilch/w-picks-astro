e19875393d0f44d54d958e9c2c9afb1929f785c2
seansusmilch <seantsusmilch@gmail.com>
2025-01-30 00:03:03 -0600

    Handle all state in the GamesProvider

--- src/components/GamesViewer/MatchupCarousel.tsx
+++ src/components/GamesViewer/MatchupCarousel.tsx
@@ -1,49 +1,15 @@
 /**
  * This takes a list of matchups and displays them in a carousel.
  */
-import {
-  Carousel,
-  CarouselContent,
-  type CarouselApi,
-} from '@/components/ui/carousel';
+import { Carousel, CarouselContent } from '@/components/ui/carousel';
 import { useGames } from './GamesProvider';
 import { GamesViewer } from './GamesViewer';
-import { useState, useEffect } from 'react';
 
 export function MatchupCarousel() {
-  const [api, setApi] = useState<CarouselApi>();
-  const [startIndex, setStartIndex] = useState(0);
-  const { games } = useGames();
-
-  useEffect(() => {
-    if (!api) return;
-
-    const url = new URL(window.location.href);
-    const pageParam = url.searchParams.get('page');
-    const gameParam = url.searchParams.get('game');
-
-    if (pageParam && gameParam) {
-      const matchupCode = `${pageParam}/${gameParam}`;
-      const idx = games.findIndex((game) => game.matchup.code === matchupCode);
-      setStartIndex(idx);
-    }
-
-    api.on('select', (e) => {
-      const idx = e.selectedScrollSnap();
-      const selectedGame = games[idx];
-      console.log(selectedGame, games);
-
-      // Update the URL search params
-      if (selectedGame) {
-        const url = new URL(window.location.href);
-        url.searchParams.set('game', selectedGame.matchup.code.split('/')[1]);
-        window.history.pushState({}, '', url);
-      }
-    });
-  }, [api, games]);
+  const { setCarouselApi, startIndex } = useGames();
 
   return (
-    <Carousel opts={{ startIndex }} setApi={setApi}>
+    <Carousel opts={{ startIndex }} setApi={setCarouselApi}>
       <CarouselContent>
         <GamesViewer />
       </CarouselContent>
--- src/components/GamesViewer/Paginator.tsx
+++ src/components/GamesViewer/Paginator.tsx
@@ -4,26 +4,25 @@ import { PaginateControls } from '../Matchup/PaginateControls';
 export function Paginator() {
   const { pages, currentPage, setCurrentPage } = useGames();
 
-  const page = pages.find((p) => p.date_code === parseInt(currentPage));
-  const nextPage = pages[pages.indexOf(page) + 1];
-  const prevPage = pages[pages.indexOf(page) - 1];
+  const pageIndex = pages.findIndex(
+    (p) => p.date_code === parseInt(currentPage)
+  );
+  if (pageIndex === -1) return null;
 
-  if (!page || !nextPage || !prevPage) return null;
+  const page = pages[pageIndex];
+  const nextPage = pages[pageIndex + 1];
+  const prevPage = pages[pageIndex - 1];
+
+  if (!page) return null;
 
   const handleNext = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
-    const url = new URL(window.location.href);
-    url.searchParams.set('page', nextPage.date_code.toString());
-    window.history.pushState({}, '', url);
-    setCurrentPage(nextPage.date_code.toString());
+    if (nextPage) setCurrentPage(nextPage.date_code.toString());
   };
 
   const handlePrev = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
-    const url = new URL(window.location.href);
-    url.searchParams.set('page', prevPage.date_code.toString());
-    window.history.pushState({}, '', url);
-    setCurrentPage(prevPage.date_code.toString());
+    if (prevPage) setCurrentPage(prevPage.date_code.toString());
   };
 
   return (
