9357eb627d37a353f487594c890ecf21202da17d
seansusmilch <seantsusmilch@gmail.com>
2025-01-30 00:15:27 -0600

    Add dark mode invert filter for Utah Jazz logo

--- src/components/NBA/Logo.tsx
+++ src/components/NBA/Logo.tsx
@@ -1,4 +1,5 @@
 import { TeamMap } from './teamMap';
+import { cn } from '@/lib/utils';
 
 export function Logo({
   tricode,
@@ -7,6 +8,18 @@ export function Logo({
   tricode: string;
   [key: string]: any;
 }) {
-  const { logo, name } = TeamMap[tricode] || TeamMap['NBA'];
-  return <img src={logo} alt={`${name} Logo`} {...props} />;
+  const { logo, name_full } = TeamMap[tricode] || TeamMap['NBA'];
+  const { className } = props;
+
+  // Apply invert filter for Utah Jazz logo in dark mode
+  const shouldInvert =
+    tricode === 'UTA' && !!document.querySelector('html.dark');
+  return (
+    <img
+      src={logo}
+      alt={`${name_full} Logo`}
+      {...props}
+      className={cn(shouldInvert ? 'dark:invert' : '', className)}
+    />
+  );
 }
