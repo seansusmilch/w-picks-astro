7f57fe81e8c390f8da7a98e4c42c9f07b4c9136e
seansusmilch <seantsusmilch@gmail.com>
2025-01-30 12:57:21 -0600

    try prefetch on tap

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ export default defineConfig({
   integrations: [tailwind({ applyBaseStyles: true }), react()],
   prefetch: {
     prefetchAll: true,
+    defaultStrategy: 'tap',
   },
   env: {
     schema: {
