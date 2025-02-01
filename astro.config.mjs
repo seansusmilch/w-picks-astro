591b791f59b03b2005afd8d591b93b20da02b5f8
seansusmilch <seantsusmilch@gmail.com>
2025-02-01 11:02:50 -0600

    Revert "Add environment-based cron job access control"
    This reverts commit ce5b924500734aa618e2ae7aa10a5def5d92fec8.

--- astro.config.mjs
+++ astro.config.mjs
@@ -44,11 +44,6 @@ export default defineConfig({
         context: 'server',
         access: 'public',
       }),
-      ENVIRONMENT: envField.string({
-        default: 'development',
-        context: 'server',
-        access: 'public',
-      }),
     },
   },
 });
