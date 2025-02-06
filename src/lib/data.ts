c8d83dae2416f1832284365f51d3341ca55e8e0a
seansusmilch <seantsusmilch@gmail.com>
2025-02-06 10:24:18 -0600

    Set auth cookie maxAge

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -26,6 +26,7 @@ export function cookieSettings({ requestUrl }: { requestUrl: string }) {
     httpOnly: true,
     sameSite: 'strict',
     path: '/',
+    maxAge: 60 * 60 * 24 * 7,
   } as AstroCookieSetOptions;
 }
 
