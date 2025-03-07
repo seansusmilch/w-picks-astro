6f0ca1e287e95a8b1dc4767b28ddf13a01f6f3a1
seansusmilch <seantsusmilch@gmail.com>
2025-03-07 14:35:22 -0600

    switch theme font to Inter

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,7 +15,7 @@ module.exports = {
     },
     extend: {
       fontFamily: {
-        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         border: 'hsl(var(--border))',
