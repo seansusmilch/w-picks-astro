44f72364f44122e3e6c0432822eda116edbc2775
seansusmilch <seantsusmilch@gmail.com>
2025-03-10 10:25:37 -0500

    fix: document not defined

--- src/components/NBA/Logo.tsx
+++ src/components/NBA/Logo.tsx
@@ -1,25 +1,32 @@
 import { TeamMap } from './teamMap';
 import { cn } from '@/lib/utils';
+import { useEffect, useState } from 'react';
 
 export function Logo({
   tricode,
+  className,
   ...props
 }: {
   tricode: string;
+  className?: string;
   [key: string]: any;
 }) {
+  const [invert, setInvert] = useState(false);
   const { logo, name_full } = TeamMap[tricode] || TeamMap['NBA'];
-  const { className } = props;
+
+  useEffect(() => {
+    if (tricode !== 'UTA') return;
+    setInvert(!!document?.querySelector('html.dark'));
+    console.log(invert);
+  }, []);
 
   // Apply invert filter for Utah Jazz logo in dark mode
-  const shouldInvert =
-    tricode === 'UTA' && !!document.querySelector('html.dark');
   return (
     <img
       src={logo}
       alt={`${name_full} Logo`}
       {...props}
-      className={cn(shouldInvert ? 'dark:invert' : '', className)}
+      className={cn(invert ? 'dark:invert' : '', className)}
     />
   );
 }
--- src/components/ui/TimeTooltip.tsx
+++ src/components/ui/TimeTooltip.tsx
@@ -1,32 +1,19 @@
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from '@/components/ui/tooltip';
 import { useFormattedDate } from '@/lib/utils';
 
 export function TimeTooltip({ time }: { time: Date }) {
   const datetime = useFormattedDate(time);
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger>
-          <div className='text-center'>
-            <p className='text-3xl font-bold tracking-tight'>
-              {datetime
-                ?.toLocaleString('en-US', {
-                  hour: 'numeric',
-                  minute: 'numeric',
-                  hour12: true,
-                })
-                .replace(' AM', 'a')
-                .replace(' PM', 'p')}
-            </p>
-          </div>
-        </TooltipTrigger>
-        <TooltipContent>{datetime?.toLocaleString('en-US')}</TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <div className='text-center'>
+      <p className='text-3xl font-bold tracking-tight'>
+        {datetime
+          ?.toLocaleString('en-US', {
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true,
+          })
+          .replace(' AM', 'a')
+          .replace(' PM', 'p')}
+      </p>
+    </div>
   );
 }
