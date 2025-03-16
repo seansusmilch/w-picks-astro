b9a3e7f5ba16a4607bd7186281b433895e5fd9ff
seansusmilch <seantsusmilch@gmail.com>
2025-03-16 17:55:33 -0500

    add icons to desktop profile menu item

--- src/components/Header/UserDropdown.tsx
+++ src/components/Header/UserDropdown.tsx
@@ -8,6 +8,7 @@ import {
 } from '@/components/ui/dropdown-menu';
 import { useState } from 'react';
 import { User } from '@/components/Header/User';
+import { UserCircle2, LogOut, Settings } from 'lucide-react';
 
 export function UserDropdown({
   username,
@@ -29,12 +30,24 @@ export function UserDropdown({
         <DropdownMenuLabel className='text-md'>My Account</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem asChild>
-          <a className='text-md' href='/profile'>
+          <a className='text-md flex items-center gap-2' href='/profile'>
+            <UserCircle2 className='h-4 w-4' />
             Profile
           </a>
         </DropdownMenuItem>
         <DropdownMenuItem asChild>
-          <a className='text-md' href='/logout' data-astro-prefetch='false'>
+          <a className='text-md flex items-center gap-2' href='/settings'>
+            <Settings className='h-4 w-4' />
+            Settings
+          </a>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          <a
+            className='text-md flex items-center gap-2'
+            href='/logout'
+            data-astro-prefetch='false'
+          >
+            <LogOut className='h-4 w-4' />
             Logout
           </a>
         </DropdownMenuItem>
