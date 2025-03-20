7f76bb32c8cdfb01b285d6335578b7ea2a965d79
seansusmilch <seantsusmilch@gmail.com>
2025-03-20 12:24:14 -0500

    refactor: scrap new games provider

--- src/stores/games.ts
+++
@@ -1,7 +0,0 @@
-import type { GameType, PageEntryType } from '@/lib/definitions';
-import { atom } from 'nanostores';
-
-export const gamesStore = atom<GameType[]>([]);
-export const pagesStore = atom<PageEntryType[]>([]);
-export const userIdStore = atom<string>('');
-export const currentPageStore = atom<string>('');
